bd787f77921e0d861434053b68f69e2194f3436e
Akhil <akhilnandu31@gmail.com>
2025-04-27 17:54:40 +0530

    firstcommit

---
+++ src/Javabasics/Car.java
@@ -0,0 +1,26 @@
+package Javabasics;
+
+public class Car{
+	  int a =10;
+	  int b=20;
+	
+
+	 Car(int g){
+		
+	  a=30;
+	//System.out.println(a);
+		b=60;
+	}
+	
+
+	public static void main (String args[]) {
+			
+			
+		Car c= new Car(40);
+	System.out.println(c.b);
+	
+		
+	
+	
+}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Javasection1 {
+}
